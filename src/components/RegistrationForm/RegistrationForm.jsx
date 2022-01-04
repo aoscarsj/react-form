1b05d735d84bcdf7625a2d4bb714a553652fb71d
Artur Oscar <Arturoscar3@gmail.com>
2022-01-04 19:48:34 -0300

    adding hooks to form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { TextField, Button, Switch, FormControlLabel } from '@mui/material';
 
 function RegistrationForm() {
+  const [name, setName] = useState('Artur');
+  const [lastName, setLastName] = useState('');
   return (
-    <form>
-      <TextField id='name' label='Name' fullWidth margin='normal' />
-      <TextField id='lastName' label='Last Name' fullWidth margin='normal' />
+    <form
+      onSubmit={(event) => {
+        console.log(name);
+
+        event.preventDefault();
+      }}
+    >
+      <TextField
+        id='name'
+        label='Name'
+        fullWidth
+        margin='normal'
+        value={name}
+        onChange={(event) => {
+          let tempName = event.target.value;
+          if (tempName.length >= 3) tempName = tempName.substring(0, 3);
+          setName(tempName);
+        }}
+      />
+      <TextField
+        value={lastName}
+        id='lastName'
+        label='Last Name'
+        fullWidth
+        margin='normal'
+      />
 
       <TextField id='cpf' label='CPF' fullWidth margin='normal' />
       <FormControlLabel
